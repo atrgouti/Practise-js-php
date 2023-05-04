c26e5eb5940955b069ae4c76e2d82cd171903e76
BILAL ATRGOUTI <bilalatrgouti2002@gmail.com>
2023-05-04 23:47:16 +0200

    done

--- guessGame/app.js
+++ guessGame/app.js
@@ -5,17 +5,34 @@ const check = document.querySelector(".check")
 const again = document.querySelector(".again")
 const input = document.querySelector(".guess")
 const score = document.querySelector(".score")
+const number = document.querySelector(".number")
+const highscore = document.querySelector(".highscore")
 let copmuterGuess;
 
 
 function onstartGame(){
     input.value = ''
+    number.textContent = "?"
+    document.body.style.backgroundColor = "#222"
     copmuterGuess = Math.floor(Math.random() * 20) + 1
     score.textContent = 20
 }
 
 function ongame(){
-    const input = document.querySelector(".guess")
+    if(input.value > copmuterGuess){
+        input.value = ''
+        number.textContent = "Low"
+        score.textContent = score.textContent - 1
+    }else if(input.value < copmuterGuess){
+        input.value = ''
+        number.textContent = "High"
+        score.textContent = score.textContent - 1
+    }else{
+        input.value = ''
+        number.textContent = "You Win"
+        document.body.style.backgroundColor = "Green"
+        highscore.textContent = score.textContent
+    }
 }
 
 
