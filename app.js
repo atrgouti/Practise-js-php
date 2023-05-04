bffbc2e84eee0359bee29fdc6bbcbf721a744dd4
BILAL ATRGOUTI <bilalatrgouti2002@gmail.com>
2023-05-04 18:53:42 +0200

    learning about npm

--- app.js
+++ app.js
@@ -19,9 +19,35 @@
 
 // secend method
 
-const whereAmI = async function(lat, lang){
-    const res = await fetch(`https://geocode.xyz/${lat},${lang}?geoit=json&auth=373951042644846129738x125239`)
-    const data = await res.json()
-    console.log(data)
-}
-whereAmI(-33.933, 18.474)
+// const whereAmI = async function(lat, lang){
+//     const res = await fetch(`https://geocode.xyz/${lat},${lang}?geoit=json&auth=373951042644846129738x125239`)
+//     const data = await res.json()
+//     console.log(data)
+// }
+// whereAmI(-33.933, 18.474)
+
+
+// const p = new Promise((resolve, reject) => {
+//     let x = 2
+//     if(x == 10){
+//         resolve("success")
+//     }else{
+//         reject("fail")
+//     }
+// })
+
+// p.then(sucess => console.log("i am in then and you are", sucess)).catch(res => console.log(`iam in then and you are ${res}`))
+
+
+const gumbling = new Promise((resolve, reject) => {
+    console.log("your game is proccesing")
+    setTimeout(function(){
+        if(Math.random() > 0.5){
+            resolve("you win")
+        }else{
+            reject("you lose")
+        }
+    }, 2000)
+})
+
+gumbling.then(res => console.log(res)).catch(res => console.log(res))
