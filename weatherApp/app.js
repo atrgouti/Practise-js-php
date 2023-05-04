9c79df7e191458a8ac62be270121702a09bf8fd1
BILAL ATRGOUTI <bilalatrgouti2002@gmail.com>
2023-05-05 01:35:52 +0200

    done

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -4,20 +4,24 @@ const city = document.querySelector(".city")
 const humidityy = document.querySelector(".humidity")
 const description = document.querySelector(".description")
 const wind = document.querySelector(".wind")
+const iconn = document.querySelector(".icon")
 
 btn.addEventListener("click", async function(){
     const input = document.querySelector(".input").value
     const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=9cae0b3c663b4bf582d225912230405&q=${input}&aqi=no`)
     const data = await res.json()
-    let {location : {name},current : {temp_c}, current: {humidity}, current: {condition: {text}}, current : {wind_kph}} = data
+    console.log(data)
+    let {location : {name},current : {temp_c}, current: {humidity}, current: {condition: {text}}, current : {wind_kph}, current: {condition: {icon}}} = data
     console.log(data)
     header.textContent = `${temp_c}°C`
     city.textContent = `Weather in ${name}`
     humidityy.textContent = `Humidity: ${humidity}%`
     description.textContent = `${text}`
     wind.textContent = `Wind speed: ${wind_kph} km/h`
-})
 
+    iconn.setAttribute("src", `${icon}`)
+
+})
 
 const getIcons = async function(){
     const res = await fetch("https://www.weatherapi.com/docs/weather_conditions.json")
