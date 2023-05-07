04368e6ba9047307a4f42c5175fb64cb5e60104a
atrgouti <bilalatrgouti2002@gmail.com>
2023-05-07 22:27:23 +0200

    make the navbar responsive

--- React-app/todoList/src/NavBar.jsx
+++ React-app/todoList/src/NavBar.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react"
 import logo from "../Photos/logo.svg"
 import linkdlen from "../Photos/linkdlen.svg"
 import github from "../Photos/github.svg"
 import contact from "../Photos/contact.svg"
 import "./Navbarstyle.css"
 export default function NavBar(){
+
+    const [isClicked, setIsClicked] = useState(false)
+
+    function handleClick(){
+        setIsClicked(!isClicked)
+    }
+
+    const divClassName = isClicked ? 'container active' : 'container';
+
     return(
-        <div className="container">
+        <div className={divClassName}>
             <div className="logo">
                 <img src={logo} alt="" />
             </div>
@@ -30,6 +40,11 @@ export default function NavBar(){
                     <p className="contactMe">Contact Me</p>
                     <img src={contact} alt="" />
                 </span></a>
+            </div>
+            <div className="burger" onClick={handleClick}>
+                <div className="line-1"></div>
+                <div className="line-2"></div>
+                <div className="line-3"></div>
             </div>
         </div>
     );
