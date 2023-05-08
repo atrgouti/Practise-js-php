b395c18f51270a13778a0af25add06a89b409259
atrgouti <bilalatrgouti2002@gmail.com>
2023-05-09 01:02:50 +0200

    responsivnese

--- React-app/todoList/src/mainSection.jsx
+++ React-app/todoList/src/mainSection.jsx
@@ -1,14 +1,26 @@
 import "./MainSectionStyle.css"
 // import MyNameLogo from "../Photos/"
-import MyNameLogo from "../Photos/MyNameLogo.svg"
+import MyNameLogo from "../Photos/MyNameLogo.png"
+import download from "../Photos/download.png"
+import main from "../Photos/main.svg"
 export default function MainSection(){
     return(
+        <>
         <div className="MainContainer">
             <div className="content">
                 <h1>Hello, I am</h1>
-                <img src={MyNameLogo} alt="" />
-                <h2>Software Engineer</h2>
+                <img className="mynamelogo" src={MyNameLogo} alt="" />
+                <h1 className="software">Software Engineer</h1>
+                <span className="resume">
+                    <a href=""><img src={download} alt="" /></a>
+                    <a href=""><p>Resume</p></a>
+                </span>
+            </div>
+            <div className="me">
+                <img src={main} alt="" />
             </div>
         </div>
+        <div className="lineOrange"></div>
+        </>
     )
 }
