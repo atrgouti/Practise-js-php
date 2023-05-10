9a75b37036e043a208a446a4f2f55b3580b0b6b5
atrgouti <bilalatrgouti2002@gmail.com>
2023-05-10 14:22:18 +0200

    added Photos

--- React-app/todoList/src/MySkills.jsx
+++ React-app/todoList/src/MySkills.jsx
@@ -1,11 +1,14 @@
 import "./mySkillsStyles.css"
+import html from "../Photos/skillIcons/html.png"
 export default function MySkills(){
     return(
         <div className="containerSkills">
             <p className="aboutme">Skills<span>()</span></p>
             <div className="wrapper">
                 <div className="left">
-                    <div className="box"></div>
+                    <div className="box">
+                        <img src={html} alt="" />
+                    </div>
                     <div className="box"></div>
                     <div className="box"></div>
                     <div className="box"></div>
